ce1820b43154104e25b6d47a1695857dc6e53068
steevejoseph <steevejoseph@knights.ucf.edu>
2017-07-15 14:53:24 -0400

    Added more functionality to bookstore, can now inquire about a book

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,8 @@
-
+import java.util.Scanner;
 public class Menu {
+
+    Scanner in = new Scanner(System.in);
+    Book nb;
 
 	Menu(){
 		System.out.println("Hello! Welcome to our bookstore!");
@@ -7,5 +10,31 @@ public class Menu {
 		System.out.println("1) Inquire about the availability of a book");
 		System.out.println("2) Buy a book");
 		System.out.println("3) Buy a cliff-noted version of a book");
+
+		int choix = in.nextInt();
+
+		if (choix == 1)
+		    one();
+
 	}
+
+	public void one(){
+        System.out.println("What is the title of the book?");
+        String tit = in.next();
+
+        System.out.println("How many pages does the book have?");
+        int pag = in.nextInt();
+
+        System.out.println("How much does the book cost?");
+        double cost = in.nextDouble();
+
+        System.out.println("How many of these books would you like?");
+        int qu = in.nextInt();
+
+        nb = new Book(tit, pag, cost, qu);
+
+        System.out.println("Here's your book! \n" + nb.toString());
+
+        System.out.println("You owe: $" + nb.sell(qu));
+    }
 }
